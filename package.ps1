a294a6d33317015a759c72208419884ef1ea644f
Dmitry Krayniy <krdima92@gmail.com>
2022-07-23 10:50:36 +0400

    Updated package script and component name

--- package.ps1
+++ package.ps1
@@ -8,6 +8,34 @@ $component = Get-Content -Path "$PSScriptRoot/component.json" | ConvertFrom-Json
 $rcImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)"
 $latestImage = "$($component.registry)/$($component.name):latest"
 
+# Copy private keys to access git repo
+if (-not (Test-Path -Path "$PSScriptRoot/docker/id_rsa")) {
+    if (-not [string]::IsNullOrEmpty($env:GIT_PRIVATE_KEY)) {
+        Write-Host "Creating docker/id_rsa from environment variable..."
+        Set-Content -Path "$PSScriptRoot/docker/id_rsa" -Value $env:GIT_PRIVATE_KEY
+    } elseif (Test-Path -Path "~/.ssh/id_rsa") {
+        Write-Host "Copying ~/.ssh/id_rsa to docker..."
+        Copy-Item -Path "~/.ssh/id_rsa" -Destination "docker"
+    } else {
+        Write-Host "Missing ~/.ssh/id_rsa file..."
+        Set-Content -Path "$PSScriptRoot/docker/id_rsa" -Value ""
+    }
+}
+
+# Copy .npmrc to docker folder to use it inside container
+if (-not (Test-Path -Path "$PSScriptRoot/docker/.npmrc")) {
+    if (-not [string]::IsNullOrEmpty($env:NPM_TOKEN)) {
+        Write-Host "Creating docker/.npmrc from environment variable..."
+        Set-Content -Path "$PSScriptRoot/docker/.npmrc" -Value "//registry.npmjs.org/:_authToken=$($env:NPM_TOKEN)"
+    } elseif (Test-Path -Path "~/.npmrc") {
+        Write-Host "Copying ~/.npmrc to docker..."
+        Copy-Item -Path "~/.npmrc" -Destination "docker" 
+    } else {
+        Write-Host "Missing ~/.npmrc file..."
+        Set-Content -Path "$PSScriptRoot/docker/.npmrc" -Value ""
+    }
+}
+
 # Build docker image
 docker build -f "$PSScriptRoot/docker/Dockerfile" -t $rcImage -t $latestImage .
 if ($LastExitCode -eq 0) {
